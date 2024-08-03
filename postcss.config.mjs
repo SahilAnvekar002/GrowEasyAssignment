e7dda0cd772c757e38a1dc1785c168b5d00a8a62
Sahil <sanvekar108@gmail.com>
2024-08-03 19:43:23 +0530

    Tailwind change 3

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,7 @@
-import { join } from 'path';
-
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {
-      config : join(__dirname, 'tailwind.config.ts')
-    },
-    autoprefixer : {}
+    tailwindcss: {},
   },
 };
 
