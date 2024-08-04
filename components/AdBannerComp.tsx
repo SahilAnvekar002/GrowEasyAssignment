2174c0344dcb223a4e9b145355aab9076bcd3ca2
Sahil <sanvekar108@gmail.com>
2024-08-04 10:58:27 +0530

    final commit

--- components/AdBannerComp.tsx
+++ components/AdBannerComp.tsx
@@ -22,13 +22,13 @@ function AdBannerComp({ id, title, des, imgUrl, bgUrl, btnText, dark, setOpenMod
   }
 
   return (
-    <div className='w-[45%] py-12 px-6 flex m-4 rounded-md shadow-md' style={{ background: `url('https://raw.githubusercontent.com/SahilAnvekar002/GrowEasyAssignment/master/public${bgUrl}')no-repeat center center/cover` }}>
+    <div className='w-[45%] py-12 px-6 flex m-4 rounded-md shadow-md' style={{ background: `url('${bgUrl}')no-repeat center center/cover` }}>
       <div className='flex flex-col w-[60%] px-10'>
         <h1 className={`text-4xl font-bold mt-4 mb-6 ${dark == "true" && 'text-white'}`}>{title}</h1>
         <span className={`text-xl mb-16 ${dark == "true" && 'text-white'}`}>{des}</span>
         <button className={`py-2 px-4 border border-gray-700 w-1/2 rounded-md ${dark == "true" && 'text-white'}`}>{btnText}</button>
       </div>
-      <div className='flex flex-row-reverse items-start w-[40%]' style={{ background: `${dark == "true" ? 'linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.5)),' : ""}url('https://raw.githubusercontent.com/SahilAnvekar002/GrowEasyAssignment/master/public${imgUrl}')no-repeat center 40%/100%` }}>
+      <div className='flex flex-row-reverse items-start w-[40%]' style={{ background: `${dark == "true" ? 'linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.5)),' : ""}url('${imgUrl}')no-repeat center 40%/100%` }}>
         <button className='-mt-8' onClick={() => handleClick(id)}><IconPencil className={`${dark == "true" && 'text-white'}`} /></button>
       </div>
     </div>
